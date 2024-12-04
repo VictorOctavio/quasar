1a3d97e33f08e0389b77088fbc508e03f4de3249
Gauna Octavio <gaunavictoroctavio@gmail.com>
2024-12-04 08:47:21 -0300

    bootstrap cols-rows grid

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,6 +1,54 @@
 <template>
-  <q-page padding>
-    <h3>hello paps</h3>
+  <q-page padding class="row">
+    <div class="col-12 col-sm-6 bg-dark">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim doloremque
+        nulla sed reiciendis dolorem delectus rem sequi voluptatem, commodi
+        rerum? Iste illum tempore dolorem sit impedit, ut unde dolorum
+        inventore.
+      </p>
+    </div>
+    <div class="col-12 col-sm-6 bg-cyan-9">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim doloremque
+        nulla sed reiciendis dolorem delectus rem sequi voluptatem, commodi
+        rerum? Iste illum tempore dolorem sit impedit, ut unde dolorum
+        inventore.
+      </p>
+    </div>
+    <div class="col-12 bg-purple-14">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim doloremque
+        nulla sed reiciendis dolorem delectus rem sequi voluptatem, commodi
+        rerum? Iste illum tempore dolorem sit impedit, ut unde dolorum
+        inventore.
+      </p>
+    </div>
+
+    <div class="row">
+      <div class="col-12 col-sm-6 bg-blue-14">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+          doloremque nulla sed reiciendis dolorem delectus rem sequi voluptatem,
+          commodi rerum? Iste illum tempore dolorem sit impedit, ut unde dolorum
+          inventore.
+        </p>
+      </div>
+      <div class="col-12 col-sm-6 bg-red-14">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+          doloremque nulla sed reiciendis dolorem delectus rem sequi voluptatem,
+          commodi rerum? Iste illum tempore dolorem sit impedit, ut unde dolorum
+          inventore.
+        </p>
+
+        <div class="q-gutter-lg">
+          <q-btn class="bg-dark text-white">More info...</q-btn>
+          <q-btn class="bg-dark text-white">More info...</q-btn>
+          <q-btn class="bg-dark text-white">More info...</q-btn>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,6 +1,18 @@
 <template>
   <q-page padding>
-    <h3>Login paps</h3>
+    <h6 class="text-primary bg-amber-14 q-ma-lg">Login paps</h6>
+    <h6 class="text-secondary">Login paps</h6>
+    <h6 class="text-accent bg-dark q-pa-lg">Login paps</h6>
+    <h6 class="text-lime-5">Login paps</h6>
+
+    <p class="bg-light-blue q-pa-sm shadow-19 text-white">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil minima
+      tempore nobis excepturi! Iure nisi itaque autem eius deleniti nihil quia
+      animi esse porro vitae consectetur dolorem, perferendis quo ea! Lorem
+      ipsum dolor sit, amet consectetur adipisicing elit. Quas perferendis sed
+      reprehenderit iste quisquam distinctio nostrum quo animi necessitatibus.
+      Quisquam dolores iure maxime soluta eos commodi quos quaerat modi sequi.
+    </p>
   </q-page>
 </template>
 
